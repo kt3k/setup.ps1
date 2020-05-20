edfd02612188b638d25f818468610e4477bd27f1
Yoshiya Hinosawa <stibium121@gmail.com>
2020-05-20 23:20:37 +0900

    first install git

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
 iwr -useb get.scoop.sh | iex
+scoop install git
 scoop bucket add extras
 scoop bucket add versions
-scoop install git
 scoop install firefox
 scoop install rustup
 scoop install python27 python
