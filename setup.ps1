90de606a29fbb146d2354f045b9a579c62f32337
Yoshiya Hinosawa <stibium121@gmail.com>
2020-10-30 00:46:34 +0900

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,19 @@
+# install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# install visual studio 2019
+choco install -y visualstudio2019community
+
+# install rustup
+choco install -y rustup.install
+
+# install git
+choco install git.install
+
+# install scoop
 iwr -useb get.scoop.sh | iex
-# TODO: add rustup
-scoop install git
 scoop bucket add extras
 scoop bucket add versions
 scoop install firefox
 scoop install python27 python
 scoop install nodejs
-scoop install deno
-scoop install gcc
