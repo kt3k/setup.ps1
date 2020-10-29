88f58ba2ca96c01891a7d6141d054b4d179b1deb
Yoshiya Hinosawa <stibium121@gmail.com>
2020-10-29 22:01:42 +0900

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,9 @@
 iwr -useb get.scoop.sh | iex
+# TODO: add rustup
 scoop install git
 scoop bucket add extras
 scoop bucket add versions
 scoop install firefox
-scoop install rustup
 scoop install python27 python
 scoop install nodejs
 scoop install deno
